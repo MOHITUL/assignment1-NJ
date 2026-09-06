7a8093c258a37fb522abf8c979eafd1e19659c47
MOHITUL <mohitulislam9678@gmail.com>
2026-09-06 23:07:02 +0600

    2nd assignment

--- answer.js
+++ answer.js
@@ -6,3 +6,28 @@ function describeValue(val) {
     return `${valueType} | ${truthiness}`;
 }
 //console.log(describeValue(25));
+
+
+//2: Bangladesh Weekend Machine
+
+function getDayType(day) {
+    const formattedDay = day.toLowerCase();
+
+    switch (formattedDay) {
+        case "friday":
+        case "saturday":
+            return "Weekend";
+        
+            case "sunday":
+        case "monday":
+        case "tuesday":
+        case "wednesday":
+        case "thursday":
+            return "Working Day";
+        
+            default:
+            return "Invalid Day";
+    }
+}
+
+//console.log(getDayType("friday"));
